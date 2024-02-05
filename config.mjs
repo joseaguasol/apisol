95b9df6fffebb2149c8b9297563007ba998b5237
Luis Alberto Gonzáles Usca (Luchin) <64605259+Luchin151090@users.noreply.github.com>
2024-02-05 12:04:47 -0500

    Update config.mjs

--- config.mjs
+++ config.mjs
@@ -8,9 +8,9 @@ const connectionStr = "postgres://postgres:1234@localhost:5432/newsol2024";
 //const connectionStr = "postgres://aguasol:TntaHgQf9msnfmHXdrQWEXHEt1hut1MC@dpg-cml86oacn0vc739oj51g-a.oregon-postgres.render.com/aguasol_ui5l";
 export const db_pool = pgp({
     connectionString: connectionStr,
-    /*ssl: {
+    ssl: {
       rejectUnauthorized: false, // Puedes ajustar esto según tus necesidades de seguridad
-    },*/
+    },
   });
 
 try{
