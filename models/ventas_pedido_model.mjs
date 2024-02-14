444ea1742a781988bc8128bde9f55499b779b9b9
joseaguasol <151564687+joseaguasol@users.noreply.github.com>
2024-02-14 16:47:08 -0500

    Update ventas_pedido_model.mjs

--- models/ventas_pedido_model.mjs
+++ models/ventas_pedido_model.mjs
@@ -126,7 +126,7 @@ const modelPedido = {
         console.log("dentro de get Pedidos para Conductores....")
 
         try {
-            const pedidos = await db_pool.any(`SELECT vp.id, vp.total, vp.fecha, vp.estado, vp.tipo, vc.nombre, vc.apellidos, vc.telefono, vc.direccion FROM ventas.ruta as vr INNER JOIN ventas.pedido as vp ON vr.id = vp.ruta_id INNER JOIN ventas.cliente as vc ON vp.cliente_id = vc.id WHERE ruta_id=$1 and conductor_id=$2`, [rutaID, conductorID]);
+            const pedidos = await db_pool.any(`SELECT vp.tipo_pago, vp.id, vp.total, vp.fecha, vp.estado, vp.tipo, vc.nombre, vc.apellidos, vc.telefono, vc.direccion FROM ventas.ruta as vr INNER JOIN ventas.pedido as vp ON vr.id = vp.ruta_id INNER JOIN ventas.cliente as vc ON vp.cliente_id = vc.id WHERE ruta_id=$1 and conductor_id=$2`, [rutaID, conductorID]);
             console.log(pedidos)
             return pedidos
 
