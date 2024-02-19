3c29838ea2ed7081c23f172be6c873ac4bbb9b0b
Luchin151090 <lgonzalesu@ulasalle.edu.pe>
2024-02-19 13:01:37 -0500

    ju

--- models/relacion_detallepedido_model.mjs
+++ models/relacion_detallepedido_model.mjs
@@ -3,7 +3,10 @@ import { db_pool } from "../config.mjs";
 const modelDetallePedido = {
     createDetallePedido: async (detalle) =>{
         try {
-            console.log("----------CREADO DE DETALLE PEDIDO-------------");
+            if (detalle.cliente_id) {
+                // Si cliente_id existe, es un cliente registrado
+                const resultado = await paquete.tx(async (t) => {
+                    console.log("----------CREADO DE DETALLE PEDIDO-------------");
             // Obtener el último ID de pedido
             const lastPedido = await db_pool.one('SELECT id FROM ventas.pedido WHERE cliente_id = $1 ORDER BY id DESC LIMIT 1',
              [detalle.cliente_id]);
@@ -14,10 +17,41 @@ const modelDetallePedido = {
             console.log("DETALLE PEDIDO INSERTADO")
             console.log(detallepedido)
             return detallepedido;
+
+                })
+                console.log(resultado)
+                return resultado
+
+
+            } else {
+                const resultado = await paquete.tx(async (t) => {
+                     // Si cliente_id es nulo, es un cliente no registrado
+                     console.log("----------CREADO DE DETALLE PEDIDO-------------");
+                     // Obtener el último ID de pedido
+                     const lastPedido = await db_pool.one('SELECT id FROM ventas.pedido WHERE cliente_nr_id = $1 ORDER BY id DESC LIMIT 1',
+                      [detalle.cliente_nr_id]);
+         
+                     const detallepedido = await db_pool.one('INSERT INTO relaciones.detalle_pedido(pedido_id, producto_id, cantidad,promocion_id) VALUES($1, $2, $3,$4) RETURNING *',
+                         [lastPedido.id, detalle.producto_id, detalle.cantidad,detalle.promocion_id]
+                     );
+                     console.log("DETALLE PEDIDO INSERTADO")
+                     console.log(detallepedido)
+                     return detallepedido;
+                })
+                return resultado
+               
+            }
+            
+            
         } catch (error) {
             throw new Error(`Error query create: ${error}`);
         }
     },
+
+
+
+
+    
 
     getDetallePedido: async () => {
         try {
