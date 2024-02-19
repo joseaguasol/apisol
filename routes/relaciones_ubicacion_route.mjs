04d0b5a4624dc36feccc65963c141f8f72513c5a
Luchin151090 <lgonzalesu@ulasalle.edu.pe>
2024-02-19 11:51:13 -0500

    update

--- routes/relaciones_ubicacion_route.mjs
+++ routes/relaciones_ubicacion_route.mjs
@@ -1,4 +1,4 @@
-import { getAllUbicaciones,createUbicacion ,getUbicacionesXCliente} from '../controllers/relaciones_ubicacion_controller.mjs';
+import { getAllUbicaciones,getUbicacionesClienteNRS,createUbicacion ,getUbicacionesXCliente} from '../controllers/relaciones_ubicacion_controller.mjs';
 
 import express from 'express';
 
@@ -7,6 +7,6 @@ const routerUbicacion= express.Router();
 routerUbicacion.post('/ubicacion',createUbicacion)
 routerUbicacion.get('/ubicacion',getAllUbicaciones)
 routerUbicacion.get('/ubicacion/:clienteID',getUbicacionesXCliente)
-
+routerUbicacion.get('/pedido_clientenr/:clienteNRID',getUbicacionesClienteNRS)
 
 export default routerUbicacion
