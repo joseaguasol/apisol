c3c239ef349b53da6b81e25fdc7d39b181286d08
Luchin151090 <lgonzalesu@ulasalle.edu.pe>
2024-02-19 11:39:08 -0500

    ubicacionnr

--- routes/ventas_pedido_route.mjs
+++ routes/ventas_pedido_route.mjs
@@ -1,10 +1,11 @@
-import { createPedidos, getLastPedidos, getPedidos,getPedidosClientes, deletePedidos,updateEstadoPedidos, updateRutaPedidos,getPedidosConductor} from '../controllers/ventas_pedido_controller.mjs';
+import { createPedidos, getLastPedidos, getPedidos,getPedidosClientes,getUbicacionesClienteNRS, deletePedidos,updateEstadoPedidos, updateRutaPedidos,getPedidosConductor} from '../controllers/ventas_pedido_controller.mjs';
 import express from 'express';
 
 const routerVentasPedido = express.Router();
 routerVentasPedido.get('/pedido',getPedidos)
 routerVentasPedido.post('/pedido',createPedidos)
 routerVentasPedido.get('/pedido_cliente/:clienteID',getPedidosClientes)
+routerVentasPedido.get('/pedido_cliente/:clienteNRID',getUbicacionesClienteNRS)
 routerVentasPedido.get('/pedido_conductor/:rutaID/:conductorID',getPedidosConductor)
 routerVentasPedido.get('/pedido_last/:clienteID',getLastPedidos)
 routerVentasPedido.delete('/pedido/:pedidoID', deletePedidos)
