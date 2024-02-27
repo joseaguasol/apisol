0ee13083d446647d1db8b6bd65a8450b8989c3d4
Luchin151090 <lgonzalesu@ulasalle.edu.pe>
2024-02-27 09:56:10 -0500

    cosillas

--- models/usuario_cliente_model.mjs
+++ models/usuario_cliente_model.mjs
@@ -43,7 +43,7 @@ const modelUserCliente = {
 
 
                     const clientes = await t.one('INSERT INTO ventas.cliente (usuario_id, nombre, apellidos, fecha_nacimiento, sexo, direccion, dni, codigo, saldo_beneficios, telefono, direccion_empresa, suscripcion, RUC, nombre_empresa, frecuencia, zona_trabajo_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16) RETURNING *',
-                        [usuario.id, cliente.nombre, cliente.apellidos, cliente.fecha_nacimiento, cliente.sexo, cliente.direccion, cliente.dni, code, cliente.saldo_beneficios, cliente.telefono, cliente.direccion_empresa, cliente.suscripcion, cliente.RUC, cliente.nombre_empresa, cliente.frecuencia, cliente.zona_trabajo_id]);
+                        [usuario.id, cliente.nombre, cliente.apellidos, cliente.fecha_nacimiento, cliente.sexo, cliente.direccion, cliente.dni, code, 0.0, cliente.telefono, cliente.direccion_empresa, cliente.suscripcion, cliente.RUC, cliente.nombre_empresa, cliente.frecuencia, cliente.zona_trabajo_id]);
 
                     console.log("cliente");
                     console.log(clientes);
